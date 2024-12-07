8be59094fda1de9e8a455e5040734fd4038225c0
ItaloCosta-dev <italocosta.dev@gmail.com>
2024-12-07 08:02:19 -0300

    🔹 feat: Criado 'data.ts' para armazenar os produtos.

---
+++ src/utils/data.ts
@@ -0,0 +1,44 @@
+export const data = {
+    products: [
+        {
+            id: '1',
+            name: 'Nome do produto',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Descrição da roupa'
+        },
+        {
+            id: '2',
+            name: 'Nome do produto',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Descrição da roupa'
+        },
+        {
+            id: '3',
+            name: 'Nome do produto',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Descrição da roupa'
+        },
+        {
+            id: '4',
+            name: 'Nome do produto',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Descrição da roupa'
+        },
+    ]
+}
