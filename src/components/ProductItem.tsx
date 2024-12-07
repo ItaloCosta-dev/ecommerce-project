1d77c3c3fa5f7220ed386f637a147535a17911a6
ItaloCosta-dev <italocosta.dev@gmail.com>
2024-12-07 09:00:55 -0300

    🔹 feat: Imagens inseridas na tela.

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,9 +1,44 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import ProductRate from './ProductRate';
 
-const ProductItem = () => {
+interface Product {
+    id: string;
+    image: string;
+    name: string;
+    rating: number;
+    numReviews: number;
+    description: string;
+    price: number
+}
+
+interface ProductItemProps {
+    product: Product
+}
+
+
+const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div>
-      
+    <div className='card'>
+      <Link href={`/product/${product.id}`}>
+        <Image 
+            src={product.image}
+            width={400}
+            height={400}
+            alt={product.name}
+            className='rounded shadow object-cover h-96 w-full'
+        />
+      </Link>
+      <div className='flex flex-col items-center justify-center p-5'>
+        <Link href={`/product/${product.id}`}>
+        <h2 className='text-lg'>{product.name}</h2>
+        </Link>
+        <ProductRate rate={product.rating} count={product.numReviews} />
+        <p className='mb-2'>{product.description}</p>
+        <p>R${product.price}</p>
+        <button>Adicionar no carrinho</button>
+      </div>
     </div>
   )
 }
